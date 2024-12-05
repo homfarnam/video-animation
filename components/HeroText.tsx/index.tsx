fee9d1789a591cca373ab6e92ddb133315ba7ef7
Farnam Homayounfard <homfarnam@gmail.com>
2024-12-05 01:14:05 +0100

    feat: add paragraph effect

--- components/HeroText.tsx/index.tsx
+++ components/HeroText.tsx/index.tsx
@@ -1,5 +1,26 @@
+import { ArrowRight } from "lucide-react";
 import { AnimationProps, motion } from "motion/react";
 
+const TextLine = ({
+  children,
+  delay,
+}: {
+  children: React.ReactNode;
+  delay: number;
+}) => (
+  <motion.span
+    className="block text-base"
+    initial={{ y: 50, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{
+      duration: 0.8,
+      delay,
+      ease: [0.43, 0.13, 0.23, 0.96],
+    }}
+  >
+    {children}
+  </motion.span>
+);
 export const HeroText = () => {
   const textVariants: AnimationProps["variants"] = {
     hidden: { y: 50, opacity: 0 },
@@ -37,7 +58,7 @@ export const HeroText = () => {
         </motion.span>
       </div>
 
-      <div className="w-full h-1/2 flex flex-col justify-center text-white">
+      <div className="w-full h-1/2 flex flex-col gap-5 justify-center text-white">
         <div className="flex flex-row items-center gap-5">
           <motion.div
             className="w-8 h-8 bg-[#0050FF] rounded-full"
@@ -45,8 +66,50 @@ export const HeroText = () => {
             animate={{ scale: 1 }}
             transition={{ delay: 0.5, duration: 0.7 }}
           />
-          <h3 className="text-2xl">Pharmaceutical</h3>
+          <motion.h3
+            className="text-2xl"
+            initial={{ x: -50, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.3, duration: 1 }}
+          >
+            Pharmaceutical
+          </motion.h3>
         </div>
+        <p className="w-1/2 text-left pr-10">
+          <TextLine delay={1.6}>
+            Bronkhorst is the leader in mass flow meters and controllers
+          </TextLine>
+          <TextLine delay={1.8}>
+            technology for gases and liquids, pressure controllers and
+            evaporation
+          </TextLine>
+          <TextLine delay={2}>
+            systems. We are the low-flow specialist.
+          </TextLine>
+        </p>
+        <motion.div
+          className="w-[400px] h-[80px] mt-5 border-2 border-white/10 backdrop-blur-[14px] bg-black/10 rounded-md p-5 flex items-center justify-between"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2.5 }}
+        >
+          <motion.span
+            className="text-base"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 2.6 }}
+          >
+            View our Gas Flow solutions
+          </motion.span>
+          <motion.div
+            //   left to right animation
+            initial={{ x: -10 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1, delay: 2.6 }}
+          >
+            <ArrowRight />
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   );
