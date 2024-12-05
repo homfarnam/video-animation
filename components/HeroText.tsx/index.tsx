773e34879ca2e529b730f11a86864d841b68dbd6
Farnam Homayounfard <homfarnam@gmail.com>
2024-12-05 01:45:03 +0100

    feat: add NavigationControls component

--- components/HeroText.tsx/index.tsx
+++ components/HeroText.tsx/index.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight } from "lucide-react";
 import { AnimationProps, motion } from "motion/react";
+import { NavigationControls } from "../NavigationControls";
 
 const TextLine = ({
   children,
@@ -9,7 +10,7 @@ const TextLine = ({
   delay: number;
 }) => (
   <motion.span
-    className="block text-base"
+    className="block text-base w-full"
     initial={{ y: 50, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{
@@ -58,58 +59,63 @@ export const HeroText = () => {
         </motion.span>
       </div>
 
-      <div className="w-full h-1/2 flex flex-col gap-5 justify-center text-white">
-        <div className="flex flex-row items-center gap-5">
+      <div className="w-full h-1/2 flex flex-row gap-5 text-white">
+        <div className="h-full w-1/2 flex flex-col justify-center gap-5">
+          <div className="flex flex-row items-center gap-5">
+            <motion.div
+              className="w-8 h-8 bg-[#0050FF] rounded-full"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ delay: 0.5, duration: 0.7 }}
+            />
+            <motion.h3
+              className="text-2xl"
+              initial={{ x: -50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.3, duration: 1 }}
+            >
+              Pharmaceutical
+            </motion.h3>
+          </div>
+          <p className="w-full text-left pr-10">
+            <TextLine delay={1.6}>
+              Bronkhorst is the leader in mass flow meters and controllers
+            </TextLine>
+            <TextLine delay={1.8}>
+              technology for gases and liquids, pressure controllers and
+              evaporation
+            </TextLine>
+            <TextLine delay={2}>
+              systems. We are the low-flow specialist.
+            </TextLine>
+          </p>
           <motion.div
-            className="w-8 h-8 bg-[#0050FF] rounded-full"
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{ delay: 0.5, duration: 0.7 }}
-          />
-          <motion.h3
-            className="text-2xl"
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ delay: 0.3, duration: 1 }}
+            className="w-[400px] h-[80px] mt-5 border-2 border-white/10 backdrop-blur-[14px] bg-black/10 rounded-md p-5 flex items-center justify-between"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 2.5 }}
           >
-            Pharmaceutical
-          </motion.h3>
-        </div>
-        <p className="w-1/2 text-left pr-10">
-          <TextLine delay={1.6}>
-            Bronkhorst is the leader in mass flow meters and controllers
-          </TextLine>
-          <TextLine delay={1.8}>
-            technology for gases and liquids, pressure controllers and
-            evaporation
-          </TextLine>
-          <TextLine delay={2}>
-            systems. We are the low-flow specialist.
-          </TextLine>
-        </p>
-        <motion.div
-          className="w-[400px] h-[80px] mt-5 border-2 border-white/10 backdrop-blur-[14px] bg-black/10 rounded-md p-5 flex items-center justify-between"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1, delay: 2.5 }}
-        >
-          <motion.span
-            className="text-base"
-            initial={{ y: 50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.8, delay: 2.6 }}
-          >
-            View our Gas Flow solutions
-          </motion.span>
-          <motion.div
-            //   left to right animation
-            initial={{ x: -10 }}
-            animate={{ x: 0 }}
-            transition={{ duration: 1, delay: 2.6 }}
-          >
-            <ArrowRight />
+            <motion.span
+              className="text-base"
+              initial={{ y: 50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.8, delay: 2.6 }}
+            >
+              View our Gas Flow solutions
+            </motion.span>
+            <motion.div
+              //   left to right animation
+              initial={{ x: -10 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 1, delay: 2.6 }}
+            >
+              <ArrowRight />
+            </motion.div>
           </motion.div>
-        </motion.div>
+        </div>
+        <div className="h-full w-1/2 flex flex-col justify-end items-end">
+          <NavigationControls />
+        </div>
       </div>
     </section>
   );
---
+++ components/NavigationControls/index.tsx
@@ -0,0 +1,34 @@
+import { motion } from "framer-motion";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+export const NavigationControls = () => {
+  return (
+    <div className="z-50 flex flex-row justify-center gap-4">
+      {/* Navigation Line */}
+
+      {/* Navigation Buttons */}
+      <motion.button
+        className="w-14 h-14 flex items-center justify-center"
+        initial={{ opacity: 0, x: +50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 1 }}
+      >
+        <ChevronLeft className="w-6 h-6 text-white" />
+      </motion.button>
+      <motion.div
+        className="h-40 w-[2px] bg-white/20"
+        initial={{ height: 0 }}
+        animate={{ height: "160px" }}
+        transition={{ duration: 1, delay: 0.5, ease: "easeInOut" }}
+      />
+      <motion.button
+        className="w-14 h-14 flex items-center justify-center"
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 1 }}
+      >
+        <ChevronRight className="w-6 h-6 text-white" />
+      </motion.button>
+    </div>
+  );
+};
